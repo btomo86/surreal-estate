fef2dac7bfe1190bd7670476a46c4d5c0a19d90d
Ben Thompson <btomo86@gmail.com>
2020-12-12 16:35:09 +0000

    add property completed just needs styling

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,10 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/AddProperty.css";
 
 const AddProperty = () => {
+  const initialState = {
+    fields: {
+      title: "",
+      city: "manchester",
+    },
+  };
+
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    console.log(fields);
+  };
+
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
   return (
-    <>
-      <div>Add Property Page</div>
-    </>
+    <div className="AddProperty">
+      <form onSubmit={handleAddProperty}>
+        <label htmlFor="title">
+          Text
+          <input
+            id="title"
+            name="title"
+            value={fields.title}
+            type="text"
+            placeholder="Add Property"
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="city">
+          City
+          <select
+            id="city"
+            name="city"
+            value={fields.city}
+            onChange={handleFieldChange}
+          >
+            <option value="Manchester">Manchester</option>
+            <option value="Leeds">Leeds</option>
+            <option value="Sheffield">Sheffield</option>
+            <option value="Liverpool">Liverpool</option>
+          </select>
+        </label>
+        <label htmlFor="type">
+          Type of Property
+          <select
+            id="type"
+            name="type"
+            value={fields.type}
+            onChange={handleFieldChange}
+          >
+            <option value="Flat">Flat</option>
+            <option value="Detached">Detached</option>
+            <option value="Semi-Detached">Semi-Detached</option>
+            <option value="Terraced">Terraced</option>
+            <option value="End of Terrace">End of Terrace</option>
+            <option value="Cottage">Cottage</option>
+            <option value="Bungalow">Bungalow</option>
+          </select>
+        </label>
+        <label htmlFor="bedrooms">
+          Bedrooms
+          <input
+            id="bedrooms"
+            name="bedrooms"
+            value={fields.bedrooms}
+            type="text"
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="bathrooms">
+          Bathrooms
+          <input
+            id="bathrooms"
+            name="bathrooms"
+            value={fields.bathrooms}
+            type="text"
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="Price">
+          Price
+          <input
+            id="price"
+            name="price"
+            value={fields.price}
+            type="text"
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            id="email"
+            name="email"
+            value={fields.email}
+            type="text"
+            onChange={handleFieldChange}
+            required
+          />
+        </label>
+        <button className="add-prop-button" type="submit">
+          Add
+        </button>
+      </form>
+    </div>
   );
 };
 
